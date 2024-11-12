94c21f602c71cb42caeb6d7ba732ef9c59a057cc
Sai Maniganahalli <168391402+saimaniganahalli@users.noreply.github.com>
2024-11-12 12:19:41 +1030

    Add files via upload

--- code.js
+++ code.js
@@ -21,3 +21,48 @@ figma.ui.onmessage = (msg) => {
         figma.ui.postMessage({ type: 'lineheight-result', value: msg.value });
     }
 };
+figma.on("selectionchange", () => {
+    const selection = figma.currentPage.selection;
+    if (selection.length === 1 && selection[0].type === "TEXT") {
+        const textNode = selection[0];
+        // Get font size and letter spacing
+        const fontSize = textNode.fontSize;
+        const letterSpacing = textNode.letterSpacing;
+        // Convert letter spacing to percentage
+        let letterSpacingPercent = 0;
+        if (typeof letterSpacing === 'number') {
+            // If letterSpacing is in pixels
+            letterSpacingPercent = (letterSpacing / fontSize) * 100;
+        }
+        else if (typeof letterSpacing === 'object' && 'unit' in letterSpacing) {
+            // If letterSpacing is a LetterSpacing object
+            if (letterSpacing.unit === 'PERCENT') {
+                letterSpacingPercent = letterSpacing.value;
+            }
+        }
+        // Get line height
+        const lineHeight = textNode.lineHeight;
+        let lineHeightValue = 0;
+        let lineHeightType = 'auto';
+        if (typeof lineHeight === 'number') {
+            // If lineHeight is in pixels
+            lineHeightValue = lineHeight;
+            lineHeightType = 'pixels';
+        }
+        else if (lineHeight && typeof lineHeight === 'object' && 'unit' in lineHeight) {
+            // If lineHeight is a LineHeight object
+            if (lineHeight.unit === 'PERCENT') {
+                lineHeightValue = lineHeight.value;
+                lineHeightType = 'percent';
+            }
+        }
+        // Send values to UI
+        figma.ui.postMessage({
+            type: 'update-values',
+            fontSize,
+            letterSpacingPercent,
+            lineHeightType,
+            lineHeightValue
+        });
+    }
+});
